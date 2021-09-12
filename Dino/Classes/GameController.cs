250b8d0a60115c5a4c2af9ec3361565238446e5b
alex.negreba@outlook.com <alex.negreba@outlook.com>
2021-09-13 01:18:11 +0700

    Добавьте файлы проекта.

---
+++ Dino/Classes/GameController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dino.Classes
+{
+    public static class GameController
+    {
+        public static Image spritesheet;
+        public static List<Road> roads;
+
+        public static int dangerSpawn = 10;
+        public static int countDangerSpawn = 0;
+
+
+        public static void Init()
+        {
+            roads = new List<Road>();
+            spritesheet = Properties.Resources.sprite;
+            GenerateRoad();
+        }
+
+        public static void MoveMap()
+        {
+            for(int i = 0; i < roads.Count; i++)
+            {
+                roads[i].transform.position.X -= 4;
+                if (roads[i].transform.position.X + roads[i].transform.size.Width < 0)
+                {
+                    roads.RemoveAt(i);
+                    GetNewRoad();
+                }
+            }
+        }
+
+        public static void GetNewRoad()
+        {
+            Road road = new Road(new PointF(0 + 100 * 9, 200), new Size(100, 17));
+            roads.Add(road);
+            countDangerSpawn++;
+
+            
+        }
+        public static void GenerateRoad()
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                Road road = new Road(new PointF(0 + 100 * i, 200), new Size(100, 17));
+                roads.Add(road);
+                countDangerSpawn++;
+            }
+        }
+
+        public static void DrawObjets(Graphics g)
+        {
+            for(int i = 0; i < roads.Count; i++)
+            {
+                roads[i].DrawSprite(g);
+            }
+        }
+    }
+}
---
+++ Dino/Classes/Physics.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dino.Classes
+{
+    public class Physics
+    {
+        public Transform transform;
+        float gravity;
+        float a;
+
+        public bool isJumping;
+        public bool isCrouching;
+
+        public Physics(PointF position, Size size)
+        {
+            transform = new Transform(position, size);
+            gravity = 0;
+            a = 0.4f;
+            isJumping = false;
+            isCrouching = false;
+        }
+
+        public void ApplyPhysics()
+        {
+            CalculatePhysics();
+        }
+
+        public void CalculatePhysics()
+        {
+            if(transform.position.Y<150 || isJumping)
+            {
+                transform.position.Y += gravity;
+                gravity += a;
+            }
+            if (transform.position.Y > 150)
+                isJumping = false;
+        }
+
+        
+
+        public void AddForce()
+        {
+            if (!isJumping)
+            {
+                isJumping = true;
+                gravity = -10;
+            }
+        }
+    }
+}
